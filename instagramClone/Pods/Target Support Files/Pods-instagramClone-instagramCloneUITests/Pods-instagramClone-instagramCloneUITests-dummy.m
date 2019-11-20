699de984eedca7bd3553f2edd35f71d1d5316e16
Sam <samroman3@gmail.com>
2019-11-20 16:57:23 -0500

    Adds podfile and firebase dependencies

---
+++ instagramClone/Pods/Target Support Files/Pods-instagramClone-instagramCloneUITests/Pods-instagramClone-instagramCloneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_instagramClone_instagramCloneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_instagramClone_instagramCloneUITests
+@end
